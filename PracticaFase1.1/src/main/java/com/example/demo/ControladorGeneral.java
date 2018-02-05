96771707bdbdccba89c5cb5c104ae785b36b9f1d
fsanchezgar <35699681+fsanchezgar@users.noreply.github.com>
2018-02-05 16:09:33 +0100

    Add files via upload

---
+++ PracticaFase1.1/src/main/java/com/example/demo/ControladorGeneral.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ControladorGeneral {
+
+	@RequestMapping("/index")
+	public String peticiones_entrantes(Model modelo) {
+		return "index";
+	}
+	
+	@RequestMapping("/ligas")
+	public String acceso_ligas(Model modelo) {
+		return "ligas";
+	}
+	
+	@RequestMapping("/clubs")
+	public String acceso_clubs(Model modelo) {
+		return "clubs";
+	}
+	
+	@RequestMapping("/calendario")
+	public String acceso_calendario(Model modelo) {
+		return "calendario";
+	}
+	
+	@RequestMapping("/noticias")
+	public String acceso_noticias(Model modelo) {
+		return "noticias";
+	}
+	
+	@RequestMapping("/contacto")
+	public String acceso_contacto(Model modelo) {
+		return "contacto";
+	}
+	
+}
